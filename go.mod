b6f32c1204d4b2691d9bd61ac8fd98cc2d508fc0
oublie6 <313166062@qq.com>
2023-03-25 20:50:52 +0800

    更新

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/oublie6/Oublie-Go
 
 go 1.18
+
+require github.com/emirpasic/gods v1.18.1
--- test.go
+++ test.go
@@ -1,3 +1,11 @@
 package Oublie_Go
 
-var A int = 1
+import "fmt"
+
+func oublie(name string) {
+	if name == "XYQ" || name == "ZYH" {
+		fmt.Println("太阳的光还耀眼吗")
+	} else {
+		fmt.Println("你好，我在等我曾经的太阳")
+	}
+}
